c59671ef22d6e415068f95d5443f14e5c6834716
ngokay <61977474+ngokay@users.noreply.github.com>
2024-09-21 02:38:42 +0700

    commit test trigger build

--- WebApplication4/Controllers/WeatherForecastController.cs
+++ WebApplication4/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace WebApplication4.Controllers
         [HttpGet(Name = "GetWeatherForecast addddd ddd  ddd")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
